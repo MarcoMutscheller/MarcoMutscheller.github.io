4554d385afac969e2319b5c980e87b84107ac41a
MarcoMutscheller <64253335+MarcoMutscheller@users.noreply.github.com>
2020-12-03 12:12:40 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -158,10 +158,3 @@ function decibelToLinear(val) {
   return Math.exp(0.11512925464970229 * val); // pow(10, val / 20)
 }
 
-$(document).ready(function(){
-  $("button").click(function(){
-    $("<div class="button" data-index="18"></div>").fadeIn();
-    $("#div2").fadeIn("slow");
-    $("#div3").fadeIn(3000);
-  });
-});
