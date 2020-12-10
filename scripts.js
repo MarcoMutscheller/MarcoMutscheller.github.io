3c19a6791e72e676ef9794ea79b8c2e59e524e3b
MarcoMutscheller <64253335+MarcoMutscheller@users.noreply.github.com>
2020-12-10 12:08:07 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -167,7 +167,29 @@ function show(id) {
 }
 
 
+function fadeIn(el){
+  el.classList.add('show');
+  el.classList.remove('hide');  
+}
 
+function fadeOut(el){
+  el.classList.add('hide');
+  el.classList.remove('show');
+}
+
+var btn = document.getElementById('btnFade'),
+    img = document.getElementById('imgFade');
+
+btn.addEventListener('click', function(){
+  if (img.className.indexOf('hide') !== -1) {
+    fadeIn(img);
+    this.innerHTML = 'Fade Out';
+  }
+  else {
+    fadeOut(img);
+    this.innerHTML = 'Fade In';
+  }
+});
 
 /*****************************Move Button*******************************************/
 
