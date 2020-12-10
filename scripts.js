4a9091282b0251931e339f161861514fbe8fa874
MarcoMutscheller <64253335+MarcoMutscheller@users.noreply.github.com>
2020-12-10 11:05:26 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -158,7 +158,7 @@ function decibelToLinear(val) {
   return Math.exp(0.11512925464970229 * val); // pow(10, val / 20)
 }
 
-/***************************************************************************/
+/********************************Interactions*******************************************/
 function show(id) {
     if(document.getElementById) {
         var interactbutton = document.getElementById(id);
@@ -166,3 +166,47 @@ function show(id) {
     }
 }
 
+/*****************************Move Button*******************************************/
+$(document).ready(function(){
+    animateDiv();
+    
+});
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(){
+    var newq = makeNewPosition();
+    var oldq = $('.a').offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    
+    $('.a').animate({ top: newq[0], left: newq[1] }, speed, function(){
+      animateDiv();        
+    });
+    
+};
+
+function calcSpeed(prev, next) {
+    
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+    
+    var greatest = x > y ? x : y;
+    
+    var speedModifier = 0.1;
+
+    var speed = Math.ceil(greatest/speedModifier);
+
+    return speed;
+
+}
