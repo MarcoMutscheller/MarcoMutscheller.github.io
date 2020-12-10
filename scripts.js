371b14773e63065d2505deaaa69aa3805a12f226
MarcoMutscheller <64253335+MarcoMutscheller@users.noreply.github.com>
2020-12-10 15:28:52 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -175,5 +175,20 @@ function show(id) {
 
 
 
-/*****************************Move Button*******************************************/
+/*****************************stop*******************************************/
+var sb = {
+  song: null,
+  init: function () {
+    sb.song = new Audio();
+    sb.listeners();
+  },
+  listeners: function () {
+    $("button").click(sb.play);
+  },
+  play: function (e) {
+    sb.song.src = e.target.value;
+    sb.song.play();
+  }
+};
 
+$(document).ready(sb.init);
