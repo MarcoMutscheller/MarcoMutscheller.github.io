6b5c4d2efed8da76e10f7d7b9892e7007dca3ae7
MarcoMutscheller <64253335+MarcoMutscheller@users.noreply.github.com>
2021-01-11 15:47:49 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -55,6 +55,86 @@ constructor(buffer, button, level = 0) {
    this.source = null;
    this.analyser = null;
 }
+     start(time, sync = true) {
+    const buffer = this.buffer;
+    let analyser = this.analyser;
+    let offset = 0;
+
+    if (analyser === null) {
+      analyser = audioContext.createAnalyser();
+      this.analyser = analyser;
+      this.analyserArray = new Float32Array(analyser.fftSize);
+    }
+
+    const gain = audioContext.createGain();
+    gain.connect(audioContext.destination);
+    gain.connect(analyser);
+
+    if (sync) {
+      // fade in only when starting somewhere in the middle
+      gain.gain.value = 0;
+      gain.gain.setValueAtTime(0, time);
+      gain.gain.linearRampToValueAtTime(this.amp, time + fadeTime);
+
+      // set offset to loop time
+      offset = (time - loopStartTime) % buffer.duration;
+    }
+
+    const source = audioContext.createBufferSource();
+    source.connect(gain);
+    source.buffer = buffer;
+    source.loop = true;
+    source.start(time, offset);
+
+    this.source = source;
+    this.gain = gain;
+
+    activeLoops.add(this);
+    this.button.classList.add('active');
+                                                                                       
+  }
+    
+      stop(time) {
+    this.source.stop(time + fadeTime);
+    this.gain.gain.setValueAtTime(this.amp, time);
+    this.gain.gain.linearRampToValueAtTime(0, time + fadeTime);
+
+    this.source = null;
+    this.gain = null;
+
+    activeLoops.delete(this);
+                                                                        
+   this.button.classList.remove('active');
+                                                                                                                            
+   this.button.style.opacity = 0.25;
+                                                                                        
+  }
+    
+     displayIntensity() {
+    const analyser = this.analyser;
+
+    if (analyser.getFloatTimeDomainData) {
+      const array = this.analyserArray;
+      const fftSize = analyser.fftSize;
+
+      analyser.getFloatTimeDomainData(array);
+
+      let sum = 0;
+      for (let i = 0; i < fftSize; i++) {
+        const value = array[i];
+        sum += (value * value);
+      }
+
+      const opacity = Math.min(1, 0.25 + 10 * Math.sqrt(sum / fftSize));
+      this.button.trans.style.opacity = opacity;
+    }
+  }
+
+  get isPlaying() {
+    return (this.source !== null);
+  }
+}
+    
 function loadMatrix1() {
   const decodeContext = new AudioContext();
 
@@ -64,7 +144,7 @@ function loadMatrix1() {
     request.responseType = 'arraybuffer';
     request.open('GET', matrix1sounds[i]);                                                     
     decodeContext.decodeAudioData(request.response, (buffer) => {
-    const button = document.querySelector(`div.button[data-index="${i}"]`);               
+    const button = document.querySelector(`div.button[name="matrix1sounds"value="${i}"]`);               
                                                                                                                
     loops1[i] = new Loop(buffer, button, levels[i])
                                                                                         
@@ -74,6 +154,8 @@ function loadMatrix1() {
     request.send();
   }
 }
+
+
     //speichere loops 0-8, aber wenn mehr als 1 Loop spielt dann stoppe 
     for (let i = 0; i < 8; i++){
     loops1[i]= i;
