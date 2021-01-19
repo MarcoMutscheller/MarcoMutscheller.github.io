c164c0d9de471cc1386b6599989bd0b79caff53d
MarcoMutscheller <64253335+MarcoMutscheller@users.noreply.github.com>
2021-01-19 15:41:11 +0100

    0

--- scripts.js
+++ scripts.js
@@ -146,7 +146,7 @@ function loadLoops() {
     request.responseType = 'arraybuffer';
     request.open('GET', sounds[i]);                                                     
     decodeContext.decodeAudioData(request.response, (buffer) => {
-      const button = document.querySelector(`div.button[data-index="${i}"]`);              
+      const button = document.querySelector(`div.button[name="matrix1sounds" value="${i}"]`);              
                                                                                                                
     loops[i] = new Loop(buffer, button, levels[i])
                                                                                         
