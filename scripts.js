ec82f18b93693a551f88d03e27f548150194486e
MarcoMutscheller <64253335+MarcoMutscheller@users.noreply.github.com>
2021-01-20 14:49:44 +0100

    k

--- scripts.js
+++ scripts.js
@@ -45,7 +45,7 @@ function show(id) {
 }
 
 $(window).on('load', function() {
-  $('button.fancy').delay(20000).fadeIn()
+  $('.fancy').delay(20000).fadeIn()
 })
 
 /*****************************stop*******************************************/
